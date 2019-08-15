1098f4657ae8121b2e7e347e8b03d209ac468f67
Aleksey K <aleksey.konovalov17@gmail.com>
2019-08-15 15:12:11 +0500

    added db , and updated favorite screen on work with db

--- lib/data/api/nasa_api.dart
+++ lib/data/api/nasa_api.dart
@@ -1,13 +1,13 @@
-import 'dart:io';
 import 'dart:convert';
-import 'package:our_mars/data/model/models.dart';
-import 'package:http/http.dart' as http;
-import 'package:our_mars/screens/screen_main/widgets_main.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:our_mars/data/db/Database.dart';
+import 'package:our_mars/data/model/models.dart';
+import 'package:our_mars/screens/screen_main/widgets_main.dart';
 
 class NasaApi {
   final api_endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers";
-  
+
   final api_path_rover_curiosity = "curiosity";
   final api_path_rover_opportunity = "opportunity";
   final api_path_rover_spirit = "spirit";
@@ -24,40 +24,46 @@ class NasaApi {
   static final mockModels = [
     PhotoModel(
       sol: 100,
-      imageUrl: 'https://mk0astronomynow9oh6g.kinstacdn.com/wp-content/uploads/2017/09/pia21850-1785MR009211_sitesphericalmosaic_noscale-full.jpg',
-      earthDate: '21-12-2018',
-      camera:  "NASD",
-    ),
-    PhotoModel(
-      sol: 100,
-      imageUrl: 'https://boygeniusreport.files.wordpress.com/2017/05/mars1.jpg?quality=98&strip=all&w=782',
+      imageUrl:
+          'https://mk0astronomynow9oh6g.kinstacdn.com/wp-content/uploads/2017/09/pia21850-1785MR009211_sitesphericalmosaic_noscale-full.jpg',
       earthDate: '21-12-2018',
       camera: "NASD",
     ),
     PhotoModel(
       sol: 100,
-      imageUrl: 'https://thenypost.files.wordpress.com/2019/03/mars-pebbles.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+      imageUrl:
+          'https://boygeniusreport.files.wordpress.com/2017/05/mars1.jpg?quality=98&strip=all&w=782',
       earthDate: '21-12-2018',
       camera: "NASD",
     ),
     PhotoModel(
       sol: 100,
-      imageUrl: 'https://www.geek.com/wp-content/uploads/2019/04/curiosity-1-625x352.jpg',
+      imageUrl:
+          'https://thenypost.files.wordpress.com/2019/03/mars-pebbles.jpg?quality=90&strip=all&w=618&h=410&crop=1',
       earthDate: '21-12-2018',
       camera: "NASD",
     ),
     PhotoModel(
       sol: 100,
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVIYGyyRdAGJ5N5WISIeinevCUezLB9MwJCE01hmh5ZJBls9ZM',
+      imageUrl:
+          'https://www.geek.com/wp-content/uploads/2019/04/curiosity-1-625x352.jpg',
       earthDate: '21-12-2018',
       camera: "NASD",
     ),
     PhotoModel(
       sol: 100,
-      imageUrl: 'https://mk0astronomynow9oh6g.kinstacdn.com/wp-content/uploads/2017/09/pia21850-1785MR009211_sitesphericalmosaic_noscale-full.jpg',
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVIYGyyRdAGJ5N5WISIeinevCUezLB9MwJCE01hmh5ZJBls9ZM',
       earthDate: '21-12-2018',
       camera: "NASD",
-    ),    
+    ),
+    PhotoModel(
+      sol: 100,
+      imageUrl:
+          'https://mk0astronomynow9oh6g.kinstacdn.com/wp-content/uploads/2017/09/pia21850-1785MR009211_sitesphericalmosaic_noscale-full.jpg',
+      earthDate: '21-12-2018',
+      camera: "NASD",
+    ),
   ];
 
   NasaApi.makeInstance();
@@ -68,12 +74,13 @@ class NasaApi {
 
   Future<List<PhotoModel>> getPhotosTest() async {
     var reqBuilder = RequestBuilder();
-    reqBuilder.setUrl(api_endpoint)
-              .addPath(api_path_rover_curiosity)
-              .addPath(api_path_photos)
-              .addParam(api_param_page, "1")
-              .addParam(api_param_earth_date, "2018-12-20")
-              .addParam(api_param_key, nasa_api_key);
+    reqBuilder
+        .setUrl(api_endpoint)
+        .addPath(api_path_rover_curiosity)
+        .addPath(api_path_photos)
+        .addParam(api_param_page, "1")
+        .addParam(api_param_earth_date, "2018-12-20")
+        .addParam(api_param_key, nasa_api_key);
     print(reqBuilder.buildRequestUrl());
     final resp = await http.get(reqBuilder.buildRequestUrl());
     if (resp.statusCode == 200) {
@@ -85,7 +92,7 @@ class NasaApi {
     } else {
       throw Exception('Failed to load photo');
     }
-  } 
+  }
 
   Future<List<PhotoModel>> getPhotos(String date, RoverType roverType) async {
     var reqBuilder = RequestBuilder();
@@ -99,12 +106,13 @@ class NasaApi {
         break;
       case RoverType.Sprit:
         reqBuilder.addPath(api_path_rover_spirit);
-        break; 
+        break;
     }
-    reqBuilder.addPath(api_path_photos)
-              .addParam(api_param_page, "1")
-              .addParam(api_param_earth_date, date)
-              .addParam(api_param_key, nasa_api_key);
+    reqBuilder
+        .addPath(api_path_photos)
+        .addParam(api_param_page, "1")
+        .addParam(api_param_earth_date, date)
+        .addParam(api_param_key, nasa_api_key);
     print(reqBuilder.buildRequestUrl());
     final resp = await http.get(reqBuilder.buildRequestUrl());
     if (resp.statusCode == 200) {
@@ -120,7 +128,6 @@ class NasaApi {
 }
 
 class RequestBuilder {
-
   String baseUrl;
   List<String> requestParams = [];
   List<String> requestPath = [];
@@ -131,18 +138,20 @@ class RequestBuilder {
   }
 
   RequestBuilder addPath(String path) {
-    this.requestPath.add("/"+path);
+    this.requestPath.add("/" + path);
     return this;
   }
 
   RequestBuilder addParam(String key, String value) {
-    this.requestParams.add(key+"="+value);
+    this.requestParams.add(key + "=" + value);
     return this;
   }
 
   String buildRequestUrl() {
     var requestPathString = "";
-    this.requestPath.forEach((path) {requestPathString += path;});
+    this.requestPath.forEach((path) {
+      requestPathString += path;
+    });
     var requestParamsString = "";
     var index = 0;
     for (final param in this.requestParams) {
@@ -153,7 +162,6 @@ class RequestBuilder {
       }
       index++;
     }
-    return this.baseUrl+requestPathString+requestParamsString; 
+    return this.baseUrl + requestPathString + requestParamsString;
   }
-
 }
--- lib/screens/screen_favorites/widgets_favorites.dart
+++ lib/screens/screen_favorites/widgets_favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:our_mars/data/db/Database.dart';
 import 'package:our_mars/data/model/models.dart';
 import 'package:our_mars/screens/screen_photo/screen_photo.dart';
 
@@ -9,7 +10,8 @@ class ImageListGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var imageItems = photos.map((model) => ImageListItem(photoModel: model, isFavorite: false,)).toList();
+    var imageItems =
+        photos.map((model) => ImageListItem(photoModel: model)).toList();
     return SliverGrid.count(
       crossAxisCount: 3,
       children: imageItems,
@@ -19,54 +21,81 @@ class ImageListGrid extends StatelessWidget {
 
 class ImageListItem extends StatelessWidget {
   final PhotoModel photoModel;
-  final bool isFavorite;
 
-  ImageListItem({Key key, this.photoModel, this.isFavorite}) : super(key: key);
+  ImageListItem({Key key, this.photoModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => ScreenPhoto(photoModel: photoModel,)));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ScreenPhoto(
+                      photoModel: photoModel,
+                    )));
       },
       child: Container(
-        margin: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          image:
-              DecorationImage(fit: BoxFit.cover, image: NetworkImage(this.photoModel.imageUrl)),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          color: Colors.redAccent,
-        ),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              bottom: 8,
-              left: 8,
-              child: StatefullFavoriteButton(isFavorite),
-            )
-          ],
-        )),
+          margin: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(this.photoModel.imageUrl)),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            color: Colors.grey,
+          ),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                bottom: 8,
+                left: 8,
+                child: StatefullFavoriteButton(photoModel),
+              )
+            ],
+          )),
     );
   }
 }
 
 class StatefullFavoriteButton extends StatefulWidget {
-  bool isFavorite;
+  PhotoModel photoModel;
 
-  StatefullFavoriteButton(this.isFavorite);
+  StatefullFavoriteButton(this.photoModel);
 
   @override
   State<StatefulWidget> createState() =>
-      StatefullFavoriteButtonState(isFavorite);
+      StatefullFavoriteButtonState(photoModel);
 }
 
 class StatefullFavoriteButtonState extends State {
-  bool isFavorite;
+  PhotoModel photoModel;
+  bool isFavorite = false;
 
-  StatefullFavoriteButtonState(this.isFavorite);
+  StatefullFavoriteButtonState(this.photoModel);
+
+  @override
+  void initState() {
+    if (photoModel.isFavorite == 1) {
+      isFavorite = true;
+    } else
+      isFavorite = false;
+    super.initState();
+  }
+
+  void updatePhotoInDB() async {
+    DBProvider.db.insertPhotoModel(photoModel);
+  }
+
 
   void onFavoritePressed() {
     isFavorite = !isFavorite;
+    if (isFavorite) {
+      photoModel.isFavorite = 1;
+    } else
+      photoModel.isFavorite = 0;
+
+    updatePhotoInDB();
+
     setState(() {});
   }
 
